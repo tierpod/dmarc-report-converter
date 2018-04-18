fe94f4d3a97446addf66a54d7fe9fa2cba1ad248
Pavel Podkorytov <pod.pavel@gmail.com>
2018-04-18 12:14:28 +0500

    Add text output

--- cmd/dmarc-report-converter/text.go
+++ cmd/dmarc-report-converter/text.go
@@ -8,27 +8,17 @@ import (
 )
 
 const textTemplate = `
-DMARC report from {{.ReportMetadata.OrgName}} ({{.ReportMetadata.Email}})
-  with id {{.ReportMetadata.ReportID}}
+DMARC report with id {{.ReportMetadata.ReportID}}
+  from {{.ReportMetadata.OrgName}} ({{.ReportMetadata.Email}})
   period {{.ReportMetadata.DateRange.Begin}}-{{.ReportMetadata.DateRange.End}}
--------------------------------------------------------------------------------
 
-Policy published for {{.PolicyPublished.Domain}}:
-  p={{.PolicyPublished.Policy}} pct={{.PolicyPublished.Pct}} adkim={{.PolicyPublished.ADKIM}} aspf={{.PolicyPublished.ASPF}}
--------------------------------------------------------------------------------
-{{ range .Record }}
-policy evaluated for source ip: {{.Row.SourceIP}} (count: {{.Row.Count}}):
-  disposition={{.Row.PolicyEvaluated.Disposition}}
-  dkim={{.Row.PolicyEvaluated.DKIM}}
-  spf={{.Row.PolicyEvaluated.SPF}}
-
-identifiers:
-  header_from={{.Identifiers.HeaderFrom}}
-
-auth_result:
-  DKIM={{.AuthResults.DKIM.Result}}
-  SPF={{.AuthResults.SPF.Result}}
----{{ end }}
+Policy published for {{.PolicyPublished.Domain}}: p={{.PolicyPublished.Policy}} pct={{.PolicyPublished.Pct}} adkim={{.PolicyPublished.ADKIM}} aspf={{.PolicyPublished.ASPF}}
+----------------------------------------------------------------------------------------------------
+{{printf "%16v|%16v|%4v|%10v|%8v|%8v|%16v|%16v" "ip"          "hostname" "msg"      "disp"                           "dkim r"                 "spf r"            "dkim domain"            "spf domain" }}
+----------------------------------------------------------------------------------------------------
+{{- range .Record }}
+{{printf "%16v|%16v|%4v|%10v|%8v|%8v|%16v|%16v" .Row.SourceIP "TODO"     .Row.Count .Row.PolicyEvaluated.Disposition .AuthResults.DKIM.Result .AuthResults.SPF.Result .AuthResults.DKIM.Domain .AuthResults.DKIM.Domain}}
+{{- end }}
 `
 
 func textOutput(d dmarc.Report, o string) error {
