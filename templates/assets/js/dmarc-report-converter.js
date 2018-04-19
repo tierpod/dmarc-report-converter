23166b34f715a2a97589e4e4dea41545c6816759
Pavel Podkorytov <pod.pavel@gmail.com>
2018-04-19 17:43:04 +0500

    Add pass/fail messages chart

--- templates/assets/js/dmarc-report-converter.js
+++ templates/assets/js/dmarc-report-converter.js
@@ -1,14 +1,20 @@
 $(document).ready(function(){
     console.log('search data and labels inside table');
 
-    // var max_items = 10;
     var data = [];
     var labels = [];
+    var auth_result_pass = 0;
+    var auth_result_fail = 0;
 
     $('#items-table tbody tr').each(function() {
-        if (max_items == 0) {
-            return false;
-        };
+        // count pass and fail auth results
+        var msgc = parseInt($(this).find('#msgc').text());
+
+        if ($(this).hasClass('auth-result-pass')) {
+            auth_result_pass = auth_result_pass + msgc;
+        } else {
+            auth_result_fail = auth_result_pass + msgc;
+        }
 
         var hostname = $(this).find('#hostname').text();
         if (hostname == "") {
@@ -17,11 +23,9 @@ $(document).ready(function(){
         var msgc = $(this).find('#msgc').text();
         labels.push(hostname);
         data.push(msgc);
-
-        //max_items--;
     });
 
-    var ctx = document.getElementById('stats-chart').getContext('2d');
+    var ctx = document.getElementById('hosts-chart').getContext('2d');
     var myChart = new Chart(ctx, {
         type: 'doughnut',
         data: {
@@ -30,25 +34,61 @@ $(document).ready(function(){
                 label: 'hostnames',
                 data: data,
                 backgroundColor: [
-                    'rgba(255, 99, 132, 0.4)',
-                    'rgba(54, 162, 235, 0.4)',
-                    'rgba(255, 206, 86, 0.4)',
-                    'rgba(75, 192, 192, 0.4)',
-                    'rgba(153, 102, 255, 0.4)',
-                    'rgba(255, 159, 64, 0.4)'
+                    'rgba(255, 99, 132, 0.6)',
+                    'rgba(54, 162, 235, 0.6)',
+                    'rgba(255, 206, 86, 0.6)',
+                    'rgba(75, 192, 192, 0.6)',
+                    'rgba(153, 102, 255, 0.6)',
+                    'rgba(255, 159, 64, 0.6)'
                 ],
-                borderColor: 'rgba(0, 0, 0, 0.1)',
                 borderWidth: 1
             }]
         },
         options: {
+            title: {
+                display: true,
+                text: 'messages count per hostnames',
+            },
             scales: {
                 yAxes: [{
-                    ticks: {
-                        beginAtZero:true
-                    }
+                    display: false,
                 }]
-            }
+            },
+            legend: {
+                display: false,
+            },
         }
+    });
+
+    var ctx = document.getElementById('stats-chart').getContext('2d');
+    var myChart = new Chart(ctx, {
+        type: 'doughnut',
+        data: {
+            labels: ['pass', 'fail'],
+            datasets: [{
+                label: 'stats',
+                data: [auth_result_pass, auth_result_fail],
+                backgroundColor: [
+                    'rgba(54, 162, 235, 0.6)',
+                    'rgba(255, 99, 132, 0.6)',
+                ],
+                borderWidth: 1
+            }],
+        },
+        options: {
+            title: {
+                display: true,
+                text: 'pass/fail messages',
+            },
+            scales: {
+                yAxes: [{
+                    display: false,
+                }]
+            },
+            legend: {
+                display: false,
+                // position: 'right'
+            },
+        },
     });
 });
