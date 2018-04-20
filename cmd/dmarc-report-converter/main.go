b54ffcb0069e7a37efbdeefe6ef9757eef734dbb
Pavel Podkorytov <pod.pavel@gmail.com>
2018-04-20 08:34:35 +0500

    close #5

--- cmd/dmarc-report-converter/main.go
+++ cmd/dmarc-report-converter/main.go
@@ -18,7 +18,7 @@ func main() {
 		flagOutputDir    string
 		flagOutputFormat string
 		flagLookupAddr   bool
-		//flagDelete       bool
+		flagDelete       bool
 	)
 
 	flag.BoolVar(&flagVersion, "version", false, "show version and exit")
@@ -26,7 +26,7 @@ func main() {
 	flag.StringVar(&flagOutputDir, "out", "", "path to output directory (if is not set, print to stdout)")
 	flag.StringVar(&flagOutputFormat, "format", "txt", "output format (text, html, json)")
 	flag.BoolVar(&flagLookupAddr, "lookup", false, "performs a reverse lookups")
-	//flag.BoolVar(&flagDelete, "delete", false, "delete source file after conversation")
+	flag.BoolVar(&flagDelete, "delete", false, "delete source file after conversation")
 	//flag.StringVar(&flagConfig, "config", "./config.yaml", "Path to config file")
 	flag.Parse()
 
@@ -58,6 +58,16 @@ func main() {
 		err = convertFile(f, cfg)
 		if err != nil {
 			log.Printf("[ERROR] %v, skip", err)
+			continue
+		}
+
+		if flagDelete {
+			log.Printf("delete %v after converting", f)
+			err = os.Remove(f)
+			if err != nil {
+				log.Printf("[ERROR] %v, skip", err)
+				continue
+			}
 		}
 	}
 }
